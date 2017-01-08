467df22567e61ce481736d4edb14459ca85893c8
phungphu <phunghuuphu@gmail.com>
2017-01-08 11:49:26 -0500

    Add files via upload

--- externaltest.js
+++ externaltest.js
@@ -1,9 +1,9 @@
 alert('JS code from: github.io');
-document.addEventListener("click", onDeviceReady, false);
+document.addEventListener("onload", onDeviceReady, false);
 function onDeviceReady() {
 	alert('alert from: github.io domain, onDeviceReady fired');
 
-	document.querySelector("#getGeoLoc").addEventListener("click", function() {
+	document.querySelector("#getGeoLoc").addEventListener("mouseover", function() {
          alert('alert from: github.io domain, document.querySelector("#getGeoLoc"). fired');});
 	       
 }
